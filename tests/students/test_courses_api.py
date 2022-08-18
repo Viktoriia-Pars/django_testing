34f9b3b61c65ff10af2f9d6be844e3203e55d77a
Viktoriia-Pars <parshenn@yandex.ru>
2022-08-18 23:48:15 +0300

    finale

--- tests/students/test_courses_api.py
+++ tests/students/test_courses_api.py
@@ -53,7 +53,7 @@ def test_get_course(client, user):
     data1 = response1.json()
     assert data[0]['id'] == 1
     assert data[0]['name'] == 'Math'
-    assert data[0]['students'] == 1
+    assert data[0]['students'] == list(cours1.students.all())
     assert data1[0]['birth_date'] == '1987-02-21'
 
 @pytest.mark.django_db
@@ -73,13 +73,82 @@ def test_get_courses(client, user, courses_factory):
         assert data[i]['name'] == courses[i].name
         assert data[i]['students'] == list(courses[i].students.all())
 
+@pytest.mark.django_db
+def test_get_course_filter_id(client, user, courses_factory):
+    # Arrange
+    courses = courses_factory(_quantity=10)
 
 
-# @pytest.mark.django_db
-# def test_create_message(client, user):
-#     count = Message.objects.count()
-#
-#     response = client.post('/messages/', data={'user': user.id, 'text': 'test text'})
-#
-#     assert response.status_code == 201
-#     assert Message.objects.count() == count + 1
+    # Act
+    id_ = courses[0].pk
+    name_ = courses[0].name
+    response = client.get(f'/api/v1/courses/?name={name_}')
+    response1 = client.get(f'/api/v1/courses/{id_}/')
+
+    # Assert
+    assert response1.status_code == 200
+    data = response.json()
+    data1 = response1.json()
+    assert len(data) == 1
+    assert data1['id'] == id_
+    assert data[0]['name'] == name_
+
+
+@pytest.mark.django_db
+def test_create_course(client, user):
+    # Arrange
+    course_data = {'name': 'History'}
+    stud_data = {'name': 'Ivan', 'birth_date':'1987-2-21'}
+
+    # Act
+
+    response = client.post('/api/v1/courses/', data=course_data)
+    response1 = client.post('/api/v1/students/', data=stud_data)
+
+    # Assert
+    assert response.status_code == 201
+    assert response1.status_code == 201
+    data = response.json()
+    data1 = response1.json()
+    assert data['name'] == 'History'
+    assert data1['name'] == 'Ivan'
+    assert data1['birth_date'] == '1987-02-21'
+
+@pytest.mark.django_db
+def test_update_course(client, user, courses_factory, students_factory):
+    # Arrange
+    courses = courses_factory(_quantity=10)
+    students = students_factory(_quantity=10)
+    course_data = {'name': 'History'}
+    stud_data = {'name': 'Ivan', 'birth_date': '1987-02-21'}
+
+    # Act
+    id_c = courses[3].pk
+    id_s = students[3].pk
+    response = client.patch(f'/api/v1/courses/{id_c}/', data=course_data)
+    response1 = client.patch(f'/api/v1/students/{id_s}/', data=stud_data)
+
+    # Assert
+    assert response.status_code == 200
+    assert response1.status_code == 200
+    data = response.json()
+    data1 = response1.json()
+    assert data['name'] == 'History'
+    assert data1['name'] == 'Ivan'
+    assert data1['birth_date'] == '1987-02-21'
+
+@pytest.mark.django_db
+def test_delete_course(client, user, courses_factory, students_factory):
+    # Arrange
+    courses = courses_factory(_quantity=10)
+    students = students_factory(_quantity=10)
+
+    # Act
+    id_c = courses[5].pk
+    id_s = students[5].pk
+    response = client.delete(f'/api/v1/courses/{id_c}/')
+    response1 = client.delete(f'/api/v1/students/{id_s}/')
+
+    # Assert
+    assert response.status_code == 204
+    assert response1.status_code == 204
